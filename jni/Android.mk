9bc2487b54efef17560c9f8cfd843fa6a56c6cd1
宋文武 <iyzsong@member.fsf.org>
2021-10-01 10:57:39 +0800

    Add 'jni' for android.

---
+++ jni/Android.mk
@@ -0,0 +1,11 @@
+LOCAL_PATH := $(call my-dir)
+CORE_DIR := $(LOCAL_PATH)/..
+
+include $(CORE_DIR)/Makefile.common
+
+include $(CLEAR_VARS)
+LOCAL_MODULE    := retro
+LOCAL_SRC_FILES := $(SOURCES_C) $(SOURCES_CXX)
+LOCAL_CFLAGS    := $(CFLAGS)
+LOCAL_LDFLAGS   := -Wl,-version-script=$(CORE_DIR)/link.T
+include $(BUILD_SHARED_LIBRARY)
