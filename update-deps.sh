38383f671b749ae85d6c72ace855a9f414c222ac
宋文武 <iyzsong@envs.net>
2022-08-04 19:08:26 +0800

    update-deps.sh: Terminate early if error happen.

--- update-deps.sh
+++ update-deps.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
+set -eu
 rm -rf deps
 mkdir -p deps
 git submodule update --init
-cp -rv "$(guix build -f deps.scm)"/* ./deps/
+cp -rv "$(guix build -f deps.scm || kill $$)"/* ./deps/
 chmod -R u+w deps
 cp -rv deps.overlay/* ./deps/
